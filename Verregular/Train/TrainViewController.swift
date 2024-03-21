74113dc8fe5c9ce7e2e3b621e889983244f3a6e9
olgazinchenko <ozinchenko.pm@gmail.com>
2024-03-21 23:48:16 +0100

    Added all necessary UI elements on the Train Screen

--- Verregular/Train/TrainViewController.swift
+++ Verregular/Train/TrainViewController.swift
@@ -8,12 +8,101 @@
 import UIKit
 
 final class TrainViewController: UIViewController {
+    private lazy var scrollView: UIScrollView = {
+        let view = UIScrollView()
+        
+        view.showsHorizontalScrollIndicator = false
+        
+        return view
+    }()
+    
+    private lazy var contentView: UIView = UIView()
+    
+    private lazy var infinitiveLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = .boldSystemFont(ofSize: 28)
+        label.textColor = .black
+        label.textAlignment = .center
+        
+        return label
+    }()
+    
+    private lazy var pastSimpleLabel: UILabel = {
+        let label = UILabel()
+        
+        label.textColor = .gray
+        label.font = .systemFont(ofSize: 14)
+        label.text = "Past Simple"
+        
+        return label
+    }()
+    
+    private lazy var participleLabel: UILabel = {
+        let label = UILabel()
+        
+        label.textColor = .gray
+        label.font = .systemFont(ofSize: 14)
+        label.text = "Past Participle"
+        
+        return label
+    }()
+    
+    private lazy var pastSimpleTextField: UITextField = {
+        let field = UITextField()
+        
+        field.borderStyle = .roundedRect
+        field.delegate = self
+        
+        return field
+    }()
+    
+    private lazy var participleTextField: UITextField = {
+        let field = UITextField()
+        
+        field.borderStyle = .roundedRect
+        field.delegate = self
+        
+        return field
+    }()
+    
+    private lazy var checkButton: UIButton = {
+        let button = UIButton()
+        
+        button.layer.cornerRadius = 10
+        button.backgroundColor = .systemGray5
+        button.setTitle("Check".localized, for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        
+        return button
+    }()
     
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Train verbs".localized
-        view.backgroundColor = .yellow
+
+        
+        setupUI()
     }
+    
+    // MARK: - Private Methods
+    private func setupUI() {
+        view.backgroundColor = .white
+        
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        contentView.addSubviews([infinitiveLabel,
+                                 pastSimpleLabel,
+                                 pastSimpleTextField,
+                                 participleLabel,
+                                 participleTextField,
+                                 checkButton])
+    }
+}
+
+// MARK: - UITextFieldDelegate
+extension TrainViewController: UITextFieldDelegate {
+    // TODO:
 }
