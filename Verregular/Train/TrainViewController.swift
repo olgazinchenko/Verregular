8212b2eb867beb02094c968c1387a3e22db4dc24
olgazinchenko <ozinchenko.pm@gmail.com>
2024-03-22 22:43:11 +0100

    Added NoNotificationCentre and UIGestureRecognizer

--- Verregular/Train/TrainViewController.swift
+++ Verregular/Train/TrainViewController.swift
@@ -6,8 +6,10 @@
 //
 
 import UIKit
+import SnapKit
 
 final class TrainViewController: UIViewController {
+    // MARK: - GUI Variables
     private lazy var scrollView: UIScrollView = {
         let view = UIScrollView()
         
@@ -24,6 +26,7 @@ final class TrainViewController: UIViewController {
         label.font = .boldSystemFont(ofSize: 28)
         label.textColor = .black
         label.textAlignment = .center
+        label.text = "Read".uppercased()
         
         return label
     }()
@@ -77,14 +80,19 @@ final class TrainViewController: UIViewController {
         return button
     }()
     
+    // MARK: - Properties
+    private let edgeInsets = 30
+
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Train verbs".localized
 
-        
         setupUI()
+        registerForKeyboardNotification()
+        unregisterForKeyboardNotification()
+        hideKeyboardWhenTappedAround()
     }
     
     // MARK: - Private Methods
@@ -99,10 +107,101 @@ final class TrainViewController: UIViewController {
                                  participleLabel,
                                  participleTextField,
                                  checkButton])
+        
+        setupConstrains()
+    }
+    
+    private func setupConstrains() {
+        scrollView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        contentView.snp.makeConstraints { make in
+            make.size.edges.equalToSuperview()
+        }
+        
+        infinitiveLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(200)
+            make.trailing.leading.equalToSuperview().inset(edgeInsets)
+        }
+        
+        pastSimpleLabel.snp.makeConstraints { make in
+            make.top.equalTo(infinitiveLabel.snp.bottom).offset(60)
+            make.trailing.leading.equalToSuperview().inset(edgeInsets)
+        }
+        
+        pastSimpleTextField.snp.makeConstraints { make in
+            make.top.equalTo(pastSimpleLabel.snp.bottom).offset(10)
+            make.trailing.leading.equalToSuperview().inset(edgeInsets)
+        }
+        
+        participleLabel.snp.makeConstraints { make in
+            make.top.equalTo(pastSimpleTextField.snp.bottom).offset(20)
+            make.trailing.leading.equalToSuperview().inset(edgeInsets)
+        }
+        
+        participleTextField.snp.makeConstraints { make in
+            make.top.equalTo(participleLabel.snp.bottom).offset(10)
+            make.trailing.leading.equalToSuperview().inset(edgeInsets)
+        }
+        
+        checkButton.snp.makeConstraints { make in
+            make.top.equalTo(participleTextField.snp.bottom).offset(100)
+            make.trailing.leading.equalToSuperview().inset(edgeInsets)
+        }
     }
 }
 
 // MARK: - UITextFieldDelegate
 extension TrainViewController: UITextFieldDelegate {
-    // TODO:
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if pastSimpleTextField.isFirstResponder {
+            participleTextField.becomeFirstResponder()
+        } else {
+            scrollView.endEditing(true)
+        }
+        
+        return true
+    }
+}
+
+// MARK: - Keyboard Events
+private extension TrainViewController {
+    func registerForKeyboardNotification() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow(_:)),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+    }
+    
+    func unregisterForKeyboardNotification() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+    }
+    
+    @objc
+    func keyboardWillShow(_ notification: Notification) {
+        guard let frame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey]
+                as? CGRect else { return }
+        
+        scrollView.contentInset.bottom = frame.height + 50
+    }
+    
+    @objc
+    func keyboardWillHide() {
+        scrollView.contentInset.bottom = .zero - 50
+    }
+    
+    func hideKeyboardWhenTappedAround() {
+        let recognizer = UITapGestureRecognizer(target: self,
+                                             action: #selector(hideKeyboard))
+        scrollView.addGestureRecognizer(recognizer)
+    }
+    
+    @objc
+    func hideKeyboard() {
+        scrollView.endEditing(true)
+    }
 }
